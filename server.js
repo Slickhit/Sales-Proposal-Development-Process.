8995295e09693a10cbce51d898e27fbc2421e8bb
Slickhit <91316272+Slickhit@users.noreply.github.com>
2025-06-21 16:25:00 -0400

    Add basic express app with deployment instructions

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 // Setting EJS as the template engine
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (if any) from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,21 +19,26 @@ app.get('/', (req, res) => {
 });
 
 app.post('/story', (req, res) => {
-    // Extracting Job_description and Cover_letter from the form
+    // Extract Job_description and Cover_letter from the form
     const jobDescription = req.body.Job_description;
     const coverLetter = req.body.Cover_letter;
 
     // If client_name is empty, default to "N/A"
-    const clientName = req.body.client_name || "N/A";
+    const clientName = req.body.client_name || 'N/A';
 
     // Pass the clientName along with other data to the result template
-    res.render('result', { 
-        Job_description: jobDescription, 
+    res.render('result', {
+        Job_description: jobDescription,
         Cover_letter: coverLetter,
-        clientName: clientName // Add this line to include the clientName
+        clientName: clientName
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+// Basic 404 handler
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
